1e9c996be453b5229b46755506b8f619fb4cd2b0
Meow <mikolaj.stanco@gmail.com>
2026-04-22 13:30:13 +0200

    FIX: Set domain URL

--- WEB/init-oauth.php
+++ WEB/init-oauth.php
@@ -3,7 +3,7 @@
 
 
     
-    $discord_url = "https://discord.com/oauth2/authorize?client_id=1101626565554618519&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8888%2Fprocess-oauth.php&scope=identify+guilds+guilds.join+gdm.join";
+    $discord_url = "https://discord.com/oauth2/authorize?client_id=1101626565554618519&response_type=code&redirect_uri=https%3A%2F%2Fstanco.pl%2Fprocess-oauth.php&scope=identify+guilds+guilds.join+gdm.join";
     header("Location: $discord_url");
     exit();
 ?>
